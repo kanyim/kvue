6b5a197f7f3be4d439949c3c12dc4d1139b21ed8
kanyim <601864391@qq.com>
2019-04-09 10:07:19 +0800

    提交模板

---
+++ gaojizujian/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="wrapper">
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+  .wrapper{
+    display:block;
+    width:1200px;
+    margin:50px auto;
+    overflow:hidden;
+    padding:10px;
+  }
+</style>
